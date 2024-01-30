b9055211746cda53badf1c6d2e8fddeedc98d590
yadsashel <ysalhi600@gmail.com>
2024-01-30 15:40:14 -0800

    0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,17 +1,2 @@
 #!/usr/bin/env ruby
-
-# Your regular expression using Oniguruma
-pattern = /School/
-
-# Accept one argument from the command line
-ARGV.each do |arg|
-  # Apply the regular expression
-  if match = arg.match(pattern)
-    # Print the matched part followed by "$"
-    puts match.to_s + "$"
-  else
-    # Print "$" if there's no match
-    puts "$"
-  end
-end
-
+puts ARGV[0].scan(/School/).join
