7d0be4743bb04f0b2c152ced2f75a80a031debd3
yadsashel <ysalhi600@gmail.com>
2024-06-29 09:58:05 -0700

    0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# This manifest creates a file at /tmp/school with specific permissions, owner, and content
+file { '/tmp/school':
+  ensure  => 'file',
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
